dee19104a4dbf9ee4137d82c9aad1dac64385b27
suyash kumar singh <suyash.singh-ext@guardhat.com>
2024-09-19 23:26:31 +0530

    added helm config

--- cards-service/src/main/java/cards/CardsServiceApplication.java
+++ cards-service/src/main/java/cards/CardsServiceApplication.java
@@ -1,13 +1,14 @@
 package cards;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
@@ -34,6 +35,7 @@ public class CardsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CardsServiceApplication.class, args);
+
     }
 
 }
--- message/src/main/java/com/bank/message/MessageApplication.java
+++ message/src/main/java/com/bank/message/MessageApplication.java
@@ -3,11 +3,19 @@ package com.bank.message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class MessageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessageApplication.class, args);
+		//SpringApplication.run(MessageApplication.class, args);
+		String date = "2020-12-23 12:34:56 +00:00".replace(" +","+").replace(" ","T") ;
+
+		OffsetDateTime dateTime = OffsetDateTime.parse(date);
+		System.out.println(dateTime);
+
 	}
 
 }
